a8fb301e0e75839788d47ea7b4ec58d1db635530
EwertonCSMelo <57071540+EwertonCSMelo@users.noreply.github.com>
2019-11-13 20:04:29 -0300

    Initial commit

---
+++ ENCEC.c
@@ -0,0 +1,8 @@
+#define tamNome 30
+
+void ENCEC(char inicio[tamNome])
+{
+  puts(inicio);
+}
+
+
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "palestrantes.c"
+#include "ENCEC.c"
+#define tamNome 30
+
+
+int main()
+{
+
+}
